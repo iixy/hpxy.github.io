61e5533355799a984ed112ca911b10fe9835d887
haopXpycary <haopxpycary@foxmail.com>
2019-01-30 18:49:10 +0800

    调参优化

--- setHeader.js
+++
@@ -1,7 +0,0 @@
-var experience = document.getElementById('experience')
-var mail = document.getElementById('mail')
-var sentence = document.getElementById('sentence')
-
-experience.style.left = window.innerWidth / 4
-mail.style.left = window.innerWidth / 4 * 2
-sentence.style.left = window.innerWidth / 4 * 3
