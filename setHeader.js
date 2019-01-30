8aca0164aff03d0b1685767aa52b1dd6d957b90e
haopXpycary <haopxpycary@foxmail.com>
2019-01-30 17:23:53 +0800

    下方导航栏

---
+++ setHeader.js
@@ -0,0 +1,8 @@
+
+var experience = document.getElementById('experience')
+var mail = document.getElementById('mail')
+var sentence = document.getElementById('sentence')
+
+experience.style.paddingLeft = window.innerWidth / 4
+mail.style.paddingLeft = window.innerWidth / 4
+sentence.style.paddingLeft = window.innerWidth / 4
